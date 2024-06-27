ed0fa29aed8259169839e4b7ae38d30d89ebe4e0
Samuel Lunion <lunion.sam@gmail.com>
2024-06-27 16:24:02 +0200

    refracto switch case I to X

--- src/main/java/romanNumber/RomanNumericalKata.java
+++ src/main/java/romanNumber/RomanNumericalKata.java
@@ -3,12 +3,29 @@ package romanNumber;
 public class RomanNumericalKata {
 
     public static int romanNumberalize(String romanNumeral) {
-        if (romanNumeral.equals("V")) {
-            return 5;
-        } else if (romanNumeral.equals("I")) {
-            return 1;
-        } else {
-            return 2;
+        switch (romanNumeral) {
+            case "I":
+                return 1;
+            case "II":
+                return 2;
+            case "III":
+                return 3;
+            case "IV":
+                return 4;
+            case "V":
+                return 5;
+            case "VI":
+                return 6;
+            case "VII":
+                return 7;
+            case "VIII":
+                return 8;
+            case "IX":
+                return 9;
+            case "X":
+                return 10;
+            default:
+                throw new IllegalArgumentException("Invalid Roman numeral: " + romanNumeral);
         }
     }
 }
--- src/test/java/romanNumber/RomanNumericalKataTest.java
+++ src/test/java/romanNumber/RomanNumericalKataTest.java
@@ -7,20 +7,38 @@ import org.junit.jupiter.api.*;
 public class RomanNumericalKataTest {
 
     @Test
-    void romanNumeralizeShouldRetrunValidForI() {
+    void romanNumeralizeShouldReturnValidForI() {
        var result = RomanNumericalKata.romanNumberalize("I");
-       assertEquals(result, 1);
-    }
-    
-    @Test
-    void romanNumeralizeShouldRetrunValidForII() {
-       var result = RomanNumericalKata.romanNumberalize("II");
-       assertEquals(result, 2);
+       assertEquals(1, result);
     }
 
     @Test
-    void romanNumeralizeShouldRetrunValidForV() {
+    void romanNumeralizeShouldReturnValidForIII() {
+       var result = RomanNumericalKata.romanNumberalize("III");
+       assertEquals(3, result);
+    }
+
+    @Test
+    void romanNumeralizeShouldReturnValidForIV() {
+       var result = RomanNumericalKata.romanNumberalize("IV");
+       assertEquals(4, result);
+    }
+
+    @Test
+    void romanNumeralizeShouldReturnValidForV() {
        var result = RomanNumericalKata.romanNumberalize("V");
-       assertEquals(result, 5);
+       assertEquals(5, result);
+    }
+    
+    @Test
+    void romanNumeralizeShouldReturnValidForVI() {
+       var result = RomanNumericalKata.romanNumberalize("VI");
+       assertEquals(6, result);
+ }
+
+    @Test
+    void romanNumeralizeShouldReturnValidForX() {
+       var result = RomanNumericalKata.romanNumberalize("X");
+       assertEquals(10, result);
     }
 }
